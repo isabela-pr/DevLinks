77f6afa108ff2f96e32fd51a7166082e21fd7573
isabela-pr <icerqueira440@gmail.com>
2023-08-10 10:55:38 -0300

    ajustes no JS e CSS

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
 var toggleMode = () => {
-    const html = document.documentElement;
+  const html = document.documentElement;
 
-    html.classList.toggle("light");
-}
+  html.classList.toggle("light");
+
+  const isLight = html.classList.contains("light");
+  const srcValue = isLight ? "../img/shrek2.png" : "../img/shrek.png";
+  const altValue = isLight
+    ? "Imagem de perfil com o personagem shrek de óculos"
+    : "Imagem de perfil com o personagem shrek";
+
+  document.querySelector("#profile img").setAttribute("src", srcValue);
+  document.querySelector("#profile img").setAttribute("alt", altValue);
+};
