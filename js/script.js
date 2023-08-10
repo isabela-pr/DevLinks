e9a2d13f3273095cfd748d68c7c5db56b430a0b8
isabela-pr <icerqueira440@gmail.com>
2023-08-10 11:26:20 -0300

    links e foto

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ var toggleMode = () => {
   html.classList.toggle("light");
 
   const isLight = html.classList.contains("light");
-  const srcValue = isLight ? "../img/shrek2.png" : "../img/shrek.png";
+  const srcValue = isLight ? "../img/WhatsApp Image 2023-08-10 at 11.20.58.jpeg" : "../img/WhatsApp Image 2023-08-10 at 11.20.59.jpeg";
   const altValue = isLight
     ? "Imagem de perfil com o personagem shrek de óculos"
     : "Imagem de perfil com o personagem shrek";
